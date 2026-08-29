717b56737d235bd04e87354e0f9330f586281314
phall168k <phall168k@gmail.com>
2026-08-29 13:04:03 +0700

    feat: share rabbitmq client

--- libs/common/src/rabbitmq/rabbitmq.module.ts
+++ libs/common/src/rabbitmq/rabbitmq.module.ts
@@ -1,4 +1,84 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RMQ_QUEUES } from './rabbitmq.constants';
+import { ConfigService } from '@nestjs/config';
+import { getRabbitMqqUrl } from './rabbitmq.config';
 
-@Module({})
+@Module({
+    imports: [
+        ClientsModule.registerAsync([
+            {
+                name: RMQ_QUEUES.AUTH,
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [getRabbitMqqUrl(configService)],
+                        queue: RMQ_QUEUES.AUTH,
+                        queueOptions: {
+                            durable: true,
+                        },
+                    },
+                }),
+            },
+            {
+                name: RMQ_QUEUES.TRIP,
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [getRabbitMqqUrl(configService)],
+                        queue: RMQ_QUEUES.TRIP,
+                        queueOptions: {
+                            durable: true,
+                        },
+                    },
+                }),
+            },
+            {
+                name: RMQ_QUEUES.BOOKING,
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [getRabbitMqqUrl(configService)],
+                        queue: RMQ_QUEUES.BOOKING,
+                        queueOptions: {
+                            durable: true,
+                        },
+                    },
+                }),
+            },
+            {
+                name: RMQ_QUEUES.PAYMENT,
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [getRabbitMqqUrl(configService)],
+                        queue: RMQ_QUEUES.PAYMENT,
+                        queueOptions: {
+                            durable: true,
+                        },
+                    },
+                }),
+            },
+            {
+                name: RMQ_QUEUES.NOTIFICATION,
+                inject: [ConfigService],
+                useFactory: (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [getRabbitMqqUrl(configService)],
+                        queue: RMQ_QUEUES.NOTIFICATION,
+                        queueOptions: {
+                            durable: true,
+                        },
+                    },
+                }),
+            },
+        ])
+    ],
+    exports: [ClientsModule],
+})
 export class RabbitmqModule {}
